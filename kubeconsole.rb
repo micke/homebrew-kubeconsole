02bfe7c4e7a8558a1f684a55afd3b7ddb6344fa3
Micke Lisinge <hi@micke.me>
2020-08-19 16:04:55 +0200

    Debug

--- kubeconsole.rb
+++ kubeconsole.rb
@@ -9,12 +9,14 @@ class Kubeconsole < Formula
   def install
     bin.install "kubeconsole"
 
+
+    raise
     # Install bash completion
-    output = Utils.safe_popen_read("./kubeconsole", "completion", "bash")
+    output = Utils.safe_popen_read("#{bin}/kubeconsole", "completion", "bash")
     (bash_completion/"kubeconsole").write output
 
     # Install zsh completion
-    output = Utils.safe_popen_read("./kubeconsole", "completion", "zsh")
+    output = Utils.safe_popen_read("#{bin}/kubeconsole", "completion", "zsh")
     (zsh_completion/"_kubeconsole").write output
   end
 
