94ababd1cf129d946adc92d2f48ad9198e1fcd77
Micke Lisinge <hi@micke.me>
2020-08-19 15:45:56 +0200

    Test fixing the completion generation

--- kubeconsole.rb
+++ kubeconsole.rb
@@ -10,11 +10,11 @@ class Kubeconsole < Formula
     bin.install "kubeconsole"
 
     # Install bash completion
-    output = Utils.safe_popen_read("#{bin}/kubeconsole", "completion", "bash")
+    output = Utils.safe_popen_read("/kubeconsole", "completion", "bash")
     (bash_completion/"kubeconsole").write output
 
     # Install zsh completion
-    output = Utils.safe_popen_read("#{bin}/kubeconsole", "completion", "zsh")
+    output = Utils.safe_popen_read("/kubeconsole", "completion", "zsh")
     (zsh_completion/"_kubeconsole").write output
   end
 
