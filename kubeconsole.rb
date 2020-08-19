4163c018402ec6028157ed1b14d071a38cb618ea
Micke Lisinge <hi@micke.me>
2020-08-19 16:43:56 +0200

    Update

--- kubeconsole.rb
+++ kubeconsole.rb
@@ -2,20 +2,15 @@ class Kubeconsole < Formula
   desc "Utility to create temporary REPL pods from deployments"
   homepage "https://github.com/micke/kubeconsole"
   url "https://github.com/micke/kubeconsole/releases/download/v1.0.0/mac.tar.gz"
-  sha256 "d98345f1401fc3a77ff2d9b1bd8eff66b37642f85010e07c5e4fc934f2188cce"
+  sha256 "ccb3c3cb79ec74394e7ca12d717aef17deb6a1010bd658c5ca0ba2fbbbcfd9b4"
 
   bottle :unneeded
 
   def install
     bin.install "kubeconsole"
 
-    # Install bash completion
-    output = Utils.safe_popen_read("#{bin}/kubeconsole", "completion", "bash")
-    (bash_completion/"kubeconsole").write output
-
-    # Install zsh completion
-    output = Utils.safe_popen_read("#{bin}/kubeconsole", "completion", "zsh")
-    (zsh_completion/"_kubeconsole").write output
+    bash_completion.install "completion/bash" => "kubeconsole"
+    zsh_completion.install "completion/zsh" => "_kubeconsole"
   end
 
   test do
