e5f529d20dde0ac30036dca61920612c7e49585e
Micke Lisinge <hi@micke.me>
2020-08-19 15:37:46 +0200

    Generate completion

--- kubeconsole.rb
+++ kubeconsole.rb
@@ -8,6 +8,14 @@ class Kubeconsole < Formula
 
   def install
     bin.install "kubeconsole"
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/kubeconsole", "completion", "bash")
+    (bash_completion/"kubeconsole").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/kubeconsole", "completion", "zsh")
+    (zsh_completion/"_kubeconsole").write output
   end
 
   test do
