5245fd4367bb28c1e7e9d80a3d4edd7bb0d5f800
Micke Lisinge <hi@micke.me>
2020-08-20 18:01:37 +0200

    Bump

--- kubeconsole.rb
+++ kubeconsole.rb
@@ -1,8 +1,8 @@
 class Kubeconsole < Formula
   desc "Utility to create temporary REPL pods from deployments"
   homepage "https://github.com/micke/kubeconsole"
-  url "https://github.com/micke/kubeconsole/releases/download/v1.0.0/mac.tar.gz"
-  sha256 "38fda706b42ec98921d82a2c7ca3a00d2617f437209b1f1e40eed82e50cd0e58"
+  url "https://github.com/micke/kubeconsole/releases/download/v1.0.1/mac.tar.gz"
+  sha256 "52233738325e11b27346f1c21674a6b5ea99a36ccc02278df589d66c060ceec7"
 
   bottle :unneeded
 
